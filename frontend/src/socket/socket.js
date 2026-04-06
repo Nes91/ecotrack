d34cdb26c781af752c4c87429ee5f5369a4effdb
Nesrine BERRA <nesrineberra@mac.home>
2026-04-06 16:21:09 +0200

    Update API_URL to REACT_APP_API_URL

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,6 +1,6 @@
 // src/socket.js
 import { io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_API_URL || 'http://localhost:8000');
+const socket = io(import.meta.env.REACT_APP_API_URL || 'http://localhost:8000');
 
 export default socket;
